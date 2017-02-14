69835135a1c1dae793c2acd214e0373be4c132d1
Axiat <nate.ortega95@gmail.com>
2017-02-14 11:22:31 -0500

    - now can identify elements to be deleted, but elements are not actually being removed from the dom

--- assests/js/project.js
+++ assests/js/project.js
@@ -116,6 +116,7 @@ function displayFistQuestion() {
 
 function displayQuestion(input) {
     "use strict";
+    updateChildQuestions();
 
     const question = input;
 
@@ -166,8 +167,6 @@ function displayQuestion(input) {
 
         document.getElementById("question-content").appendChild(question_div);   // add the new div to the page
     }
-    updateChildQuestions(question);
-
 
 }
 
@@ -209,29 +208,51 @@ function displayPictures(input) {
  * if a questions' answer is changed, we delete all questions on the page that are
  * not directly a child of the question answered.
  */
-function updateChildQuestions(input) {
+function updateChildQuestions() {
     "use strict";
 
-    const question = input;
     let all_questions = document.getElementsByClassName("question");
     let length = all_questions.length;
+    let should_delete = false; // boolean flag
 
-    const children = dict[question];
-
-    console.log("selected: " + question);
-    if(children !== undefined ){
 
         for( let i = 0; i < length ; i++ ){
-            const curr_question = all_questions[i].firstChild; // grabs the question name from the label
-            const text = curr_question.innerHTML;
 
-            if(!children.includes(text) && text !== question){
-                console.log("doesn't belong: " + text);
+            const curr_question = all_questions[i]; // grabs the question name from the label
+            const label = curr_question.children[0].innerHTML;
+            const selected = curr_question.children[1].value;
+
+            const curr_children = dict[selected]; // get the children of the choice
+
+
+            if(!should_delete){
+
+                if( i + 1 <= length){ // prevent index out of bounds
+
+                    const next_question = all_questions[i+1];
+
+                    if(next_question !== undefined) {
+                        const next_label = next_question.children[0].innerHTML;
+
+
+                        if(!curr_children.includes(next_label) && next_label !== selected){
+                            should_delete = true;
+
+                        }
+
+                    }
+
+                }
+
             }
-        }
-    }
+            else{
+                console.log("deleting: " + label);
 
-    console.log("----------");
+            }
+
+
+        }
+
 
 }
 
